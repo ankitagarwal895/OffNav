e4ec70cdeafcfb00a5b0b1c1c4a21d3f8eb884fb
ankitagarwal895 <ankit.agarwal895@gmail.com>
2016-06-18 19:01:48 +0530

    Message Parsed, Directions received in json

--- OffNav_Server/API/views.py
+++ OffNav_Server/API/views.py
@@ -9,12 +9,18 @@ import requests
 
 SID="ACa38fdeeb011037900e62f2b1177ea6ec"
 AUTH_TOKEN="6df2b92c807d6f48fa7aa1794a8b376a"
+urls=[]
+urls.append('https://maps.googleapis.com/maps/api/directions/json?origin=')
+urls.append('&destination=')
+urls.append('&key=AIzaSyBPfBImZ8ALTSY07YIwTNSd9g3tpx_foSc')
 
 @api_view(['POST'])
 def directions(request):
     a=request.data.get('From')
     b=request.data.get('Body')
+    temp=b.split('&')
+    url=urls[0]+temp[0]+urls[1]+temp[1]+urls[2]
+    res=requests.get(url)
     data={"Body":b,"From":"+12513331811","To":a}
-
     #res=requests.post('https://api.twilio.com/2010-04-01/Accounts/'+SID+"/Messages.json", auth=HTTPBasicAuth(SID,AUTH_TOKEN),data=data)
-    return Response(data)
+    return Response(res)
