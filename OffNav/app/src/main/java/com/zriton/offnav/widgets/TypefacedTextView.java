c1cc943620f043ff18a0cbc0777d11a07a272b73
aditya <jainadi011@gmail.com>
2016-06-18 18:56:33 +0530

    Adapter adn UI added

---
+++ OffNav/app/src/main/java/com/zriton/offnav/widgets/TypefacedTextView.java
@@ -0,0 +1,30 @@
+package com.zriton.offnav.widgets;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+
+public class TypefacedTextView extends TextView {
+
+    public TypefacedTextView(final Context context, final AttributeSet attrs) {
+
+        super(context, attrs);
+
+        if (isInEditMode()) {
+            return;
+        }
+        if (attrs != null) {
+            final int typefaceCode = TypefaceUtils.typefaceCodeFromAttribute(context, attrs);
+
+            final Typeface typeface = TypefaceCache
+                            .get(context.getAssets(), typefaceCode);
+            setTypeface(typeface);
+        }
+    }
+
+    public TypefacedTextView(final Context context) {
+        super(context);
+    }
+}
